ebbc39c0a89c280fe5cdbcd990ac471ff7699ee0
anna <gurtovenko.anna.v@gmail.com>
2025-04-15 16:53:18 +0300

    delete port

--- terraform/modules/ecs/services/main.tf
+++ terraform/modules/ecs/services/main.tf
@@ -52,7 +52,6 @@ resource "aws_ecs_service" "this" {
   service_registries {
     registry_arn = var.cloud_map_service_arn
     container_name = var.service_name
-    container_port = var.container_port
   }
 
   depends_on = [aws_ecs_task_definition.this]
