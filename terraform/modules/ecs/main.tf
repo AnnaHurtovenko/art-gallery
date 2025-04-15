6982d0c208e093fef2d7291fde9889b82ddb8911
anna <gurtovenko.anna.v@gmail.com>
2025-04-15 16:24:35 +0300

    edit

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -2,6 +2,10 @@ resource "aws_cloudwatch_log_group" "ecs_logs" {
   name              = "/ecs/art-gallery"
   retention_in_days = 14
 
+  lifecycle {
+    ignore_changes = [name]
+  }
+
   tags = {
     Environment = var.environment
     Project     = var.project_tag
